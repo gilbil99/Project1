08dd4571702cfb898c729dd9d50fa606fc69d903
HongKilDong <golbol@naver.com>
2024-01-25 14:45:27 +0900

    나누기 추가 계산기 완성

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def sub_func(n1, n2) :
 def mul_func(n1, n2) :
     
     return n1*n2
-
+def div_func(n1, n2) :
+    
+    return n1/n2
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 ## 메인 코드부
@@ -17,4 +19,6 @@ print(num1, "+", num2, "=", res)
 res = sub_func(num1, num2)
 print(num1, "-", num2, "=", res)
 res = mul_func(num1, num2)
-print(num1, "-", num2, "=", res)
+print(num1, "*", num2, "=", res)
+res = div_func(num1, num2)
+print(num1, "/", num2, "=", res)
